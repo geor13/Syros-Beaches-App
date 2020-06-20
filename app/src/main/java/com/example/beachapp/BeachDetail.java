5a3676c100446ec17a482263b64dbde976a75a14
Georgios <g_aggelou@yahoo.com>
2020-06-20 16:56:30 +0300

    the end !!!

--- app/src/main/java/com/example/beachapp/BeachDetail.java
+++ app/src/main/java/com/example/beachapp/BeachDetail.java
@@ -1,9 +1,11 @@
 package com.example.beachapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +16,7 @@ public class BeachDetail extends AppCompatActivity {
     private TextView onoma;
     private TextView perigrafi;
     private ImageView beachImage;
+    private TextView otherPotions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +26,26 @@ public class BeachDetail extends AppCompatActivity {
         Intent intent = getIntent();
         paralia = (SingleBeach)intent.getSerializableExtra("THE_BEACH");
 
-        onoma = (TextView)findViewById(R.id.name_beach);
-        perigrafi = (TextView)findViewById(R.id.details_beach);
-        beachImage = (ImageView)findViewById(R.id.image_testing);
+        Toolbar topBar = (Toolbar) findViewById(R.id.DetailTopBar);
+        setSupportActionBar(topBar);
+
+        onoma = (TextView)findViewById(R.id.textView5);
+        perigrafi = (TextView)findViewById(R.id.textView_Description);
+        beachImage = (ImageView)findViewById(R.id.imageView_PhotoBeach);
+        otherPotions = (TextView)findViewById(R.id.GoToMain);
 
         onoma.setText(paralia.getName());
         perigrafi.setText(paralia.getDescription());
 
         Glide.with(this).load(paralia.getView()).into(beachImage);
+
+        otherPotions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BeachDetail.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
